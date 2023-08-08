4cdff81ef8cf2a854ba0b0f77461e35858ffe2e2
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-08-08 15:17:20 -0300

    feat: creat show post function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,19 @@ const posts = [];
 function savesPost() {
     const title = document.getElementById("title").value;
     const category = document.getElementById("category").value;
-    const resume = document.getElementById("resumo").value;
+    const resumo = document.getElementById("resumo").value;
     const author = document.getElementById("author").value;
     const date = document.getElementById("date").value;
 
     
-    if(title && category && resume && author && date) {
-        storePosts(title, category, resume, author, date)
+    if(title && category && resumo && author && date) {
+        storePosts(title, category, resumo, author, date)
+        cleanFields();
+        showPosts();
     } else{
         alert("Preencha todos os campos!")
     }
-    cleanFields()
+    
 }
 
 function cleanFields() {
@@ -24,16 +26,36 @@ function cleanFields() {
     document.getElementById("date").value = "";
 }
 
-function storePosts(title, category, resume, author, date) {
+function storePosts(title, category, resumo, author, date) {
     const post = {
         title: title,
         category: category,
-        resume: resume,
+        resumo: resumo,
         author: author,
         date: date,
     };
     posts.push(post)
 
     console.log(posts)
+}
+
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="itemPost">
+        <h2>${post.title}</h2>
+        <p><strong>Categoria: </strong>${post.category}</p>
+        <p><strong>Resumo: </strong>${post.resumo}</p>
+        <p><strong>Autor: </strong>${post.author}</p>
+        <p><strong>Data de publicação: </strong>${post.date}</p>
+
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="deletePost(${index})">Excluir</button>
+        </div>
+        `;
+    });
+    document.getElementById("list").innerHTML = showContent
 }
 
