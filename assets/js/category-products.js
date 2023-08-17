9e00214e8f87da923bfda24da2d0ed8871a5ae91
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-08-17 10:21:35 -0300

    feat: add class product

--- assets/js/category-products.js
+++ assets/js/category-products.js
@@ -1,5 +1,16 @@
 class Category {
-    constructor(name) {
+    constructor(id, name) {
+        this.id = id;
         this.name = name;
+        this.products = []
+    }
+}
+
+class Prpducts {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
     }
 }
