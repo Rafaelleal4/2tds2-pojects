624d0ca2b0f65dad106fd546d9cf9b2df264a8d0
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-08-17 11:17:04 -0300

    feat: creat class  ProductService

--- assets/js/category-products.js
+++ assets/js/category-products.js
@@ -6,7 +6,7 @@ class Category {
     }
 }
 
-class Products {
+class Product {
     constructor(id, name, price, category) {
         this.id = id;
         this.name = name;
@@ -18,11 +18,34 @@ class Products {
 class CategoryService {
     constructor(){
         this.categories = [];
-        this.nextCategoryId = 1;
+        this.nextCategoryId = 0;
     }
     addCategory(name) {
         const id = this.nextCategoryId++;
         const category = new Category(id, name);
         this.categories.push(category);
     }
+}
+
+class ProductsService {
+    constructor() {
+        this.products = [];
+        this.nextProductId = 0;
+    }
+    addProduct(name, price, category){
+        const id = this.nextProductId++;
+        const product = new Product(id, name, category);
+        category.products.push(product)
+        this.products.push(product)
+    }
+}
+
+const categoryService = new CategoryService()
+
+function creatCategory() {
+    const categoryName = "Cenoura";
+    
+    categoryService.addCategory(categoryName)
+
+    console.log(categoryService.categories)
 }
