13535998f54d37f2689c4e809fb98a95b33f72eb
Rafael Leal <rafael.leal5@aluno.senai.br>
2023-08-17 10:07:47 -0300

    feat: Finish exercise

--- assets/js/script.forEach.js
+++ assets/js/script.forEach.js
@@ -1,23 +1,82 @@
-// // const companies = ["Apple", "Google", "Facebook"];
+// // // // const companies = ["Apple", "Google", "Facebook"];
 
-// // companies.forEach(company => {
-// //     console.log(`Hey, ${company}!`)
+// // // // companies.forEach(company => {
+// // // //     console.log(`Hey, ${company}!`)
+// // // // })
+
+// // // const names = ["Whinds", "Freeway", "Teste", "Maria"];
+
+// // // names.forEach((name) => {
+// // //     console.log(`Hi, ${name}!`)
+// // // })
+
+// // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+// // console.log(numbers);
+
+// // numbers.forEach((number, index) => {
+// //     if (number % 2 == 0) {
+// //         console.log(number)
+// //         console.log(`O ${number} está na posição ${index}`)
+// //     }
+    
 // // })
 
-// const names = ["Whinds", "Freeway", "Teste", "Maria"];
+// const cars = [
+//     {
+//         marca: "Ford",
+//         modelo: "Focus",
+//     },
+//     {
+//         marca: "BMW",
+//         modelo: "BMW Z4",
+//     },
+//     {
+//         marca: "Fiat",
+//         modelo: "Palio",
+//     },
+//     {
+//         marca: "Audi",
+//         modelo: "A3",
+//     },
+// ];
 
-// names.forEach((name) => {
-//     console.log(`Hi, ${name}!`)
-// })
+// cars.forEach((car) => {
+//     console.log(`Marca: ${car.marca} - Modelo: ${car.modelo}`);
+// });
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-
-console.log(numbers);
-
-numbers.forEach((number, index) => {
-    if (number % 2 == 0) {
-        console.log(number)
-        console.log(`O ${number} está na posição ${index}`)
+class Car {
+    constructor(marca, modelo){
+        this.marca = marca;
+        this.modelo = modelo;
     }
-    
-})
+}
+
+class ListCars {
+    constructor(){
+        this.cars = []
+    }
+    addCar(car){
+        this.cars.push(car)
+    }
+};
+
+const listaDeCarros = new ListCars()
+
+function fip(){
+    let inputModel = document.getElementById("modelo").value;
+    let inputBrand = document.getElementById("marca").value;
+
+    const meuCarro = new Car(inputBrand, inputModel);
+
+    listaDeCarros.addCar(meuCarro);
+
+    console.log(listaDeCarros)
+
+    clearCar();
+}
+
+function clearCar(){
+    document.getElementById("marca").value = "";
+    document.getElementById("modelo").value = "";
+}
